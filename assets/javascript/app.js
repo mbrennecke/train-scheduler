4cc8cf0687d4e1ca7cd6ab8e10b4744d5043b29a
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-16 19:18:39 -0600

    minute countdown works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+var firstTimeConverted;
+ var tRemainder;
+ var frequency;
+ var trainName;
+ var trainDestination;
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCYmOMRPExjjShhR9LEHPn-dmf7DqD66lY",
@@ -28,20 +34,71 @@ $("#submitBtn").on("click", function(event) {
         rate: rate,
         dateAdded: firebase.database.ServerValue.TIMESTAMP,
     });
+	$("#name").val("");
+    $("#destination").val("");
+    $("#time").val("");
+    $("#rate").val("");
+	
 
-})
+});
+
+
+function updateTime(){
+	var query = database.ref().orderByKey();
+	query.once("value").then(function(snapshot){
+		snapshot.forEach(function(childSnapshot) {
+	frequency = childSnapshot.val().rate;
+	
+	trainName = childSnapshot.val().name;
+	trainDestination = childSnapshot.val().destination;
+    
+	displayTable();
+	});
+	});
+}
+
+function displayTable(){
+	
+    // Current Time
+    var currentTime = moment();
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	// Time apart (remainder)
+	var tRemainder = diffTime % frequency;
+	// Minutes until next train
+	var tMinutesTillTrain = frequency - tRemainder;
+	// Next Train
+	var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	 var newRow = "<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td class='text-center'>" + frequency + "</td><td class='text-center'>" + moment(nextTrain).format("h:mm a") + "</td><td class='text-center'>" + tMinutesTillTrain + "</td></tr>";
+	
+	$("#current").append(newRow);
+}
+
+    
+   
 
 database.ref().on("child_added", function(childSnapshot){
+	// First Time (pushed back 1 year to make sure it comes before current time)
+    firstTimeConverted = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
+
+	frequency = childSnapshot.val().rate;
+	
+	trainName = childSnapshot.val().name;
+	trainDestination = childSnapshot.val().destination;
     
-    var newRow = "<tr><td>" + childSnapshot.val().name + "</td><td>" + childSnapshot.val().role + "</td><td>" + childSnapshot.val().startDate + "</td><td>12</td><td>" + childSnapshot.val().rate + "</td><td>12</td></tr>";
+	displayTable()
+    
+});
 
-    $("#current").append(newRow);
-})
+	function run() {
+		$("#current").empty();
+      clearInterval(intervalId);
+      var intervalId = setInterval(updateTime, 60*1000);
+    }
 
-
-
-
-})
+	run();
+	
+});
 
 
 
